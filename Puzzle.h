cf99cded241776fce68e4cf6ec796eb80b425357
Mason Prosser <mprosser@nd.edu>
2015-02-22 16:13:56 -0500

    Added the Puzzle.h file to the repository

---
+++ Puzzle.h
@@ -0,0 +1,13 @@
+/* Mason Prosser		2/24/15
+Puzzle.h
+Puzzle class to hold a Sudoku board and play a simple game */
+
+#ifndef PUZZLE_H
+#define PUZZLE_H
+
+class Puzzle{
+  public:
+  private:
+};
+
+#endif
