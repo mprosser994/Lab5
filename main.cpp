c47551252ae44623610873817f2a092a89770935
Mason Prosser <mprosser@nd.edu>
2015-02-22 19:22:37 -0500

    Added a Makefile, a driver program, and some sample input. Modified the puzzle class to try to fix issues with reading in files, but it still seg faults.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/* Mason Prosser		2/25/15
+main.cpp
+Driver program to read in different Sudoku and Wordoku puzzles and print them to the screen */
+
+#include "Puzzle.h"
+
+int main(){
+  Puzzle<int> sudoku;
+
+  sudoku.print();
+}
